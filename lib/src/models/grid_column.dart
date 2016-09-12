21eed405f366040a431d456043d30745a10a76c2
ranquild <ranquild@gmail.com>
2016-09-12 08:51:48 +0300

    Add default services to grid column metadata

--- lib/src/models/grid_column.dart
+++ lib/src/models/grid_column.dart
@@ -1,8 +1,13 @@
+import 'package:pivot/src/encoders/html_value_encoder.dart';
 import 'package:pivot/src/encoders/value_encoder.dart';
+import 'package:pivot/src/formatters/text_value_formatter.dart';
 import 'package:pivot/src/formatters/value_formatter.dart';
+import 'package:pivot/src/presenters/empty_value_presenter.dart';
 import 'package:pivot/src/presenters/value_presenter.dart';
+import 'package:pivot/src/providers/self_value_provider.dart';
 import 'package:pivot/src/providers/value_provider.dart';
 import 'package:pivot/src/sanitizers/value_sanitizer.dart';
+import 'package:pivot/src/sanitizers/white_space_sanitizer.dart';
 
 class GridColumn {
   final String id;
@@ -18,10 +23,10 @@ class GridColumn {
     this.id,
     this.title,
     this.width,
-    this.provider,
-    this.formatter,
-    this.sanitizer,
-    this.encoder,
-    this.presenter
+    this.provider: const SelfValueProvider(),
+    this.formatter: const TextValueFormatter(),
+    this.sanitizer: const WhiteSpaceSanitizer(),
+    this.encoder: const HtmlValueEncoder(),
+    this.presenter: const EmptyValuePresenter()
   });
 }
