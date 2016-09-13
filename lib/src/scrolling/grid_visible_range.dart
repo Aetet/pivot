e22e39a95c85c8e31251e89588946f69c403fae2
ranquild <ranquild@gmail.com>
2016-09-13 22:04:26 +0300

    Override equality operator for grid visible range

--- lib/src/scrolling/grid_visible_range.dart
+++ lib/src/scrolling/grid_visible_range.dart
@@ -1,6 +1,18 @@
+import 'package:quiver/core.dart';
+
 class GridVisibleRange {
   final int first;
   final int last;
 
   GridVisibleRange(this.first, this.last);
+
+  @override
+  bool operator ==(dynamic other) {
+    return other is GridVisibleRange && first == other.first && last == other.last;
+  }
+
+  @override
+  int get hashCode {
+    return hash2(first, last);
+  }
 }
