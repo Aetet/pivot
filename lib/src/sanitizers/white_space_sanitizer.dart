656aa5a582428339aefa82085369e8c4abeeb1b8
ranquild <ranquild@gmail.com>
2016-09-15 21:13:45 +0300

    Fix whitespace sanitizer issue with replacing new lines

--- lib/src/sanitizers/white_space_sanitizer.dart
+++ lib/src/sanitizers/white_space_sanitizer.dart
@@ -1,7 +1,7 @@
 import 'package:pivot/src/sanitizers/value_sanitizer.dart';
 
 class WhiteSpaceSanitizer implements ValueSanitizer {
-  static final RegExp _whiteSpacePattern = new RegExp('\s');
+  static final RegExp _whiteSpacePattern = new RegExp('[^\\S\n]');
 
   const WhiteSpaceSanitizer();
 
---
+++ test/sanitizers/white_space_sanitizer_test.dart
@@ -0,0 +1,18 @@
+import 'package:test/test.dart';
+import 'package:pivot/src/sanitizers/white_space_sanitizer.dart';
+
+void main() {
+  WhiteSpaceSanitizer sanitizer = new WhiteSpaceSanitizer();
+
+  test('should not change empty strings', () {
+    expect(sanitizer.sanitize(''), equals(''));
+  });
+
+  test('should replace whitespace with regular spaces but not new lines', () {
+    expect(sanitizer.sanitize(' \t\n\f '), equals('  \n  '));
+  });
+
+  test('should replace unicode spaces with regular spaces but not new lines', () {
+    expect(sanitizer.sanitize('\u00a0\u180e\n\u2003'), equals('  \n '));
+  });
+}
