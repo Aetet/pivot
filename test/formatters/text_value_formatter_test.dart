ad637d08f4f02be92ffec6d651565ccef240c8e9
ranquild <ranquild@gmail.com>
2016-09-16 19:23:15 +0300

    Add tests for text formatter

---
+++ test/formatters/text_value_formatter_test.dart
@@ -0,0 +1,18 @@
+import 'package:pivot/src/formatters/text_value_formatter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  TextValueFormatter formatter = new TextValueFormatter();
+
+  test('should format passed test as string', () {
+    expect(formatter.getText('text'), 'text');
+  });
+
+  test('should format passed number as string', () {
+    expect(formatter.getText(42), '42');
+  });
+
+  test('should format null value as empty string', () {
+    expect(formatter.getText(null), '');
+  });
+}
