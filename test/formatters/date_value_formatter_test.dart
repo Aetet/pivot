2d41482f02812bf73080e0ea1c012e28b3776095
ranquild <ranquild@gmail.com>
2016-09-16 19:27:18 +0300

    Add tests for date formatter

---
+++ test/formatters/date_value_formatter_test.dart
@@ -0,0 +1,17 @@
+import 'package:pivot/src/formatters/date_value_formatter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  DateValueFormatter formatter = new DateValueFormatter('MM/dd/yyyy');
+
+  test('should format milliseconds as date with pattern', () {
+    DateTime date = new DateTime(2000, 10, 15);
+    int milliseconds = date.millisecondsSinceEpoch;
+
+    expect(formatter.getText(milliseconds), '10/15/2000');
+  });
+
+  test('should format null value as empty string', () {
+    expect(formatter.getText(null), '');
+  });
+}
