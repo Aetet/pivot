ef3aaf708f48f6caf5f98c25e06eb1af23a2b7ce
ranquild <ranquild@gmail.com>
2016-09-16 18:47:27 +0300

    Add tests for key valyue provider

---
+++ test/providers/key_value_provider_tests.dart
@@ -0,0 +1,18 @@
+import 'package:pivot/src/providers/key_value_provider.dart';
+import 'package:test/test.dart';
+
+void main() {
+  KeyValueProvider provider = new KeyValueProvider('id');
+
+  test('should extract value by key', () {
+    expect(provider.getValue({ 'id': 1 }), equals(1));
+  });
+  
+  test('should extract null value by key', () {
+    expect(provider.getValue({ 'id': null }), null);
+  });
+  
+  test('should extract missing value as null by key', () {
+    expect(provider.getValue({ 'title': 'text' }), null);
+  });
+}
