f00e6d58d90e3c491cc97e3afb27bdbb8687dd2b
ranquild <ranquild@gmail.com>
2016-09-17 19:17:23 +0300

    Add tests for scrolling service

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,4 +1,6 @@
 import 'package:mockito/mockito.dart';
 import 'package:pivot/pivot.dart';
 
+class GridLayoutServiceMock extends Mock implements GridLayoutService {}
+
 class GridRenderingServiceMock extends Mock implements GridRenderingService {}
