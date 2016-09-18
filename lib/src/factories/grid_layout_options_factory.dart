de96d3a4cc299db98884c0a74b9d8814af756142
ranquild <ranquild@gmail.com>
2016-09-18 15:02:25 +0300

    Adjust default grid row height

--- lib/src/factories/grid_layout_options_factory.dart
+++ lib/src/factories/grid_layout_options_factory.dart
@@ -2,7 +2,7 @@ import 'package:pivot/src/layout/grid_layout_options.dart';
 
 GridLayoutOptionsFactory() {
   return const GridLayoutOptions(
-    rowHeight: 25,
+    rowHeight: 26,
     borderWidth: 1,
     paddingTop: 2,
     paddingBottom: 2,
